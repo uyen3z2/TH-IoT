4944565ebf02a5cd81aefa52d739ade458b520f4
uyen3z2 <ddan.dhkm16a3hn@sv.uneti.edu.vn>
2025-11-09 09:54:09 +0700

    Add files via upload

---
+++ bai5/src/main.cpp
@@ -0,0 +1,130 @@
+//LẬP TRÌNH WIFI ACCESS POINT
+
+#include <WiFi.h> 
+const char* ssid = "ESP32-Wifi-AP-Mode";
+const char* password = "12345678";
+//Tạo một web server tại cổng 80 - cổng mặc định cho web 
+WiFiServer webServer(80); 
+//khai báo chân sử dụng led  
+const int led1 = 2;  
+String led1Status = "OFF";  
+String header;  
+unsigned long currentTime = millis(); 
+// Previous time 
+unsigned long previousTime = 0;  
+// Define timeout time in milliseconds (example: 1000ms = 1s) 
+const long timeoutTime = 1000; 
+void setup() { 
+Serial.begin(115200);  
+pinMode(led1, OUTPUT); 
+// Set outputs to LOW 
+digitalWrite(led1, LOW);  
+Serial.print("Setting AP mode"); 
+WiFi.softAP(ssid, password);  
+IPAddress IP = WiFi.softAPIP(); //mặc định là 192.168.4.1 
+Serial.print("AP IP address: "); 
+Serial.println(IP); 
+//khởi tạo webserver 
+ 
+  webServer.begin(); 
+} 
+  
+void loop() { 
+  WiFiClient webClient = webServer.available();  
+  if(webClient) 
+  { 
+    //khoi tao gia tri ban dau cho time 
+    currentTime = millis(); 
+    previousTime = currentTime; 
+    Serial.println("New web Client"); 
+    //biến lưu giá trị response 
+    String currentLine = ""; 
+    //nếu có client connect và không quá thời gian time out 
+   while(webClient.connected()&&currentTime - previousTime <= timeoutTime) 
+    { 
+      //đọc giá trị timer tại thời điểm hiện tại 
+      currentTime = millis(); 
+      //nếu client còn kết nối 
+      if(webClient.available()) 
+      { 
+        //đọc giá trị truyền từ client theo từng byte kiểu char 
+        char c = webClient.read(); 
+        Serial.write(c); 
+        header += c; // lưu giá trị vào Header 
+        if(c == '\n')  
+        //Nếu đọc được kí tự xuống dòng (hết chuỗi truyền tới) 
+        { 
+          if (currentLine.length() == 0)  
+          { 
+            // HTTP headers luôn luôn bắt đầu với code HTTP 
+         // ví dụ HTTP/1.1 200 OK) 
+            webClient.println("HTTP/1.1 200 OK"); 
+            webClient.println("Content-type:text/html");  
+ 
+    // sau đó là kiểu nội dụng mà client gửi tới, ví dụ này là html 
+            webClient.println("Connection: close");  
+    // kiểu kết nối ở đây là close.  
+    // Nghĩa là không giữ kết nối sau khi nhận bản tin 
+            webClient.println();  
+            // nếu trong file header có giá trị 
+            if (header.indexOf("GET /led1/on") >= 0)  
+            { 
+              Serial.println("Led1 on"); 
+              led1Status = "on"; 
+              digitalWrite(led1, HIGH); 
+            }  
+            else if (header.indexOf("GET /led1/off") >= 0)  
+            { 
+              Serial.println("Led1 off"); 
+              led1Status = "off"; 
+              digitalWrite(led1, LOW); 
+            }             
+            // Response trang HTML  
+            webClient.println("<!DOCTYPE html><html>"); 
+            webClient.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"); 
+            //thêm font-awesome  
+            webClient.println("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/fontawesome.min.css\">");           
+            // Web Page Heading H1 with CSS 
+            webClient.println("<body><h1 style=\"color:Tomato\">ESP32 Access Point Web Server</h1>");  
+            // Web Page Heading H2 
+            webClient.println("<h2 style=\"color:#077a39\"><a href=\"https://khoadientu.uneti.edu.vn\">THUC TAP LAP TRINH IOT</a></h2>"); 
+            webClient.println("<i class=\"fa fa-home\" ariahidden=\"true\"></i>");  
+            // Display current state, and ON/OFF buttons for Led1 
+ 
+            webClient.println("<p>Led1 - State " + led1Status + "</p>"); 
+            // If the Led1Status is off, it displays the ON button        
+            if (led1Status=="off")  
+            { 
+              //khởi tạo một nút nhấn có đường dẫn đích là /led1/on 
+            webClient.println("<p><a href=\"/led1/on\"><button class=\"button\">ON</button></a></p>"); 
+            }  
+            else 
+            { 
+              //khởi tạo một nút nhấn có đường dẫn đích là /led1/off 
+            webClient.println("<p><a href=\"/led1/off\"><button class=\"button button2\">OFF</button></a></p>"); 
+            }            
+            webClient.println("</body></html>");  
+            // The HTTP response ends with another blank line 
+            webClient.println(); 
+            // Break out of the while loop 
+            break; 
+          } 
+          else 
+          { 
+            currentLine = ""; 
+          } 
+        } 
+        else if (c != '\r')   //nếu giá trị gửi tới khác xuống dòng 
+        { 
+          currentLine += c;     //lưu giá trị vào biến 
+        } 
+      } 
+    } 
+    // Xoá header để sử dụng cho lần tới 
+    header = ""; 
+    // ngắt kết nối 
+webClient.stop(); 
+Serial.println("Client disconnected."); 
+Serial.println(""); 
+} 
+}
